7f05d3f059a84ad6a110c58f8d693d8ebc5bb12f
7TEP <moussadiegane.sarr@uadb.edu.sn>
2020-11-12 00:58:45 +0100

    Projet creation d'API avec laravel

---
+++ app/Http/Controllers/TopicalityController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\Topicality as ResourcesTopicality;
+use App\Models\Topicality;
+use Facade\FlareClient\Http\Response;
+use Illuminate\Http\Request;
+
+class TopicalityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return  ResourcesTopicality::collection(Topicality::orderByDesc('created_at')->get());   
+
+        //return $topicality->toJson(JSON_PRETTY_PRINT);
+    }
+
+ 
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        if(Topicality::create($request->all())){
+            return response()->json(
+                [
+                    'success' => "Actualite cree avec succes !",
+                ] , 200
+            );
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Topicality  $topicality
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Topicality $topicality)
+    {
+        return new ResourcesTopicality($topicality);
+    }
+
+    
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Topicality  $topicality
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Topicality $topicality)
+    {
+        if($topicality->update($request->all())){
+            return response()->json(
+                [
+                    'success' => "Actualite modifiee avec succes !",
+                ], 200
+            );
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Topicality  $topicality
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Topicality $topicality)
+    {
+        if($topicality->delete()){
+            return response()->json([
+                'success' => 'Actualite supprimee avec success'
+            ]);
+        }
+    }
+}
